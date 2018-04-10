b508d9c9bc0caa025d141edfab2547c2e48c2689
Sebastian Ullrich <sebasti@nullri.ch>
2018-04-10 18:59:12 +0200

    Travis: use cargo instead of cross

--- ci/before_deploy.sh
+++ ci/before_deploy.sh
@@ -17,10 +17,8 @@ main() {
 
     test -f Cargo.lock || cargo generate-lockfile
 
-    # TODO Update this to build the artifacts that matter to you
-    cross rustc --bin elan-init --target $TARGET --release -- -C lto
+    cargo rustc --bin elan-init --target $TARGET --release -- -C lto
 
-    # TODO Update this to package the right artifacts
     cp target/$TARGET/release/elan-init $stage/
 
     cd $stage
