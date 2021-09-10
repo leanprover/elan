5fe253b2c8a3c1fd8fd65265c48a3862285ad137
Sebastian Ullrich <sebasti@nullri.ch>
2021-09-10 09:56:50 +0200

    fix: update URL on not-Linux, again

--- src/elan-dist/build.rs
+++ src/elan-dist/build.rs
@@ -5,7 +5,7 @@ use std::path::PathBuf;
 
 fn main() {
     let out_dir = PathBuf::from(env::var_os("OUT_DIR").unwrap());
-    let target = env::var("RELEASE_TARGET_NAME").or(env::var("TARGET")).unwrap();
+    let target = env::var("RELEASE_TARGET_NAME").unwrap();
 
     File::create(out_dir.join("target.txt")).unwrap().write_all(target.as_bytes()).unwrap();
     println!("leanpkg:rerun-if-changed=build.rs");
