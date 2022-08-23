af23a1ef506bd78a281d9d5678bd4d3426aead04
Kiel Hurley <kiel.hurley@canterbury.ac.nz>
2022-08-23 16:12:20 +1200

    Whitespace

--- elan-init.ps1
+++ elan-init.ps1
@@ -1,40 +1,47 @@
 <#
 .SYNOPSIS
     .
+
 .DESCRIPTION
     This is just a little script that can be downloaded from the Internet to
     install elan. It just does platform detection, downloads the latest
     installer, then runs it.
+
 .PARAMETER Verbose
     Produce verbose output about the elan installation process.
+
 .PARAMETER NoPrompt
     Do not present elan installation menu of choices.
+
 .PARAMETER NoModifyPath
     Do not modify PATH environment variable.
+
 .PARAMETER DefaultToolchain
     Which tool chain to setup as your default toolchain, or specify 'none'
+
 .PARAMETER ElanRoot
     Where to find the elan-init tool, default is https://github.com/leanprover/elan/releases
+
 .PARAMETER ElanVersion
     Specific version of elan to download and run instead of latest, e.g. 1.4.1
 #>
 param(
-    [bool]$Verbose = 0,
-    [bool]$NoPrompt = 0,
-    [bool]$NoModifyPath = 0,
-    [string]$DefaultToolchain = "",
-    [string]$ElanRoot = "https://github.com/leanprover/elan/releases",
-    [string]$ElanVersion = ""
+    [bool] $Verbose = 0,
+    [bool] $NoPrompt = 0,
+    [bool] $NoModifyPath = 0,
+    [string] $DefaultToolchain = "",
+    [string] $ElanRoot = "https://github.com/leanprover/elan/releases",
+    [string] $ElanVersion = ""
 )
 
-$cputype=[System.Environment]::GetEnvironmentVariable("PROCESSOR_ARCHITECTURE");
+$cputype = [System.Environment]::GetEnvironmentVariable("PROCESSOR_ARCHITECTURE");
 
 if ($cputype -ne "AMD64") {
     Write-Host "### Elan install only supports 64-bit Windows with AMD64 architecture"
     return 1
 }
 
-$_arch="x86_64-pc-windows-msvc"
+$_arch = "x86_64-pc-windows-msvc"
 $_ext = ".exe"
 $temp = [System.IO.Path]::GetTempPath()
 $_dir = Join-Path $temp "elan"
@@ -66,13 +73,13 @@ $cmdline = " "
 if ($DefaultToolchain -ne "") {
     $cmdline += "--default-toolchain $DefaultToolchain"
 }
-if ($NoPrompt){
+if ($NoPrompt) {
     $cmdline += " -y"
 }
-if ($NoModifyPath){
+if ($NoModifyPath) {
     $cmdline += " --no-modify-path"
 }
-if ($Verbose){
+if ($Verbose) {
     $cmdline += " --verbose"
 }
 $details = Start-Process -FilePath "$_file" -ArgumentList $cmdline -Wait -NoNewWindow -Passthru
