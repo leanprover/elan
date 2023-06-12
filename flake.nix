b8357f7ca6e30f9ed386e4c5424efc9b39a7a773
Sebastian Ullrich <sebasti@nullri.ch>
2023-06-12 12:04:57 +0200

    chore: add flake dev shell

---
+++ flake.nix
@@ -0,0 +1,26 @@
+{
+  description = "A Lean version manager";
+
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+  inputs.flake-utils.url = "github:numtide/flake-utils";
+
+  outputs = { self, nixpkgs, flake-utils, ... }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let pkgs = import nixpkgs { inherit system; }; in {
+        devShell = pkgs.mkShell {
+          nativeBuildInputs = with pkgs; [
+            rustc
+            cargo
+            gcc
+          ];
+
+          RUST_SRC_PATH = pkgs.rust.packages.stable.rustPlatform.rustLibSrc;
+
+          buildInputs = with pkgs; [
+            openssl.dev
+            pkg-config
+          ];
+        };
+      }
+    );
+}
