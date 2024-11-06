012b434bcd4f10f6194bbf8aa0df60fce6cfeecb
Mix <32300164+mnixry@users.noreply.github.com>
2024-11-06 22:12:54 +0800

    feat: support for toolchain origins with underscores (#138)
    related: #99 
    
    Currently, the remote toolchain reference feature on GitHub does not
    support repositories with underscores in their names. This limitation
    affects repositories like `leanprover-community/mathematics_in_lean`.

--- src/elan/toolchain.rs
+++ src/elan/toolchain.rs
@@ -37,7 +37,7 @@ pub struct ComponentStatus {
 }
 
 pub fn lookup_toolchain_desc_ext(cfg: &Cfg, name: &str, no_net: bool) -> Result<ToolchainDesc> {
-    let pattern = r"^(?:([a-zA-Z0-9-]+[/][a-zA-Z0-9-]+)[:])?([a-zA-Z0-9-.]+)$";
+    let pattern = r"^(?:([a-zA-Z0-9-_]+[/][a-zA-Z0-9-_]+)[:])?([a-zA-Z0-9-.]+)$";
 
     let re = Regex::new(pattern).unwrap();
     if let Some(c) = re.captures(name) {
